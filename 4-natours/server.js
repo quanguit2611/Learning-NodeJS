91abea2e4b3b1d6819e6932bccb4f75719b678f0
quanguit2611 <18521300@gm.uit.edu.vn>
2023-04-26 21:12:40 +0700

    add mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,10 +1,29 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-console.log(process.env);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successfully established');
+  })
+  .catch((err) => {
+    console.log('DB connection error');
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

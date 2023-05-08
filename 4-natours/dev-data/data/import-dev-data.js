278769efee8b914d1f6ba8321aef19a5478a4e9a
quanguit2611 <18521300@gm.uit.edu.vn>
2023-05-08 18:16:57 +0700

    a script to import data in terminal

---
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB connection successfully established');
+  })
+  .catch((err) => {
+    console.log('DB connection error');
+    console.log(err);
+  });
+
+//READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+//IMPORT DATA TO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+//DETELE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);

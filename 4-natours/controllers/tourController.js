03a9e36fbbbfcf9fe669e135a9637aea2165da33
quanguit2611 <18521300@gm.uit.edu.vn>
2023-05-31 20:30:42 +0700

    delete some old comments on code

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -42,49 +42,6 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
 
-    // // BUILD QUERY
-    // // 1a) Filtering
-    // const queryObj = { ...req.query };
-    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    // excludedFields.forEach((field) => delete queryObj[field]);
-
-    // // 1b) Advance filtering
-
-    // let queryStr = JSON.stringify(queryObj);
-    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr));
-
-    // let query = Tour.find(JSON.parse(queryStr));
-
-    // // 2) Sorting
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   query = query.sort(sortBy);
-    // } else {
-    //   query = query.sort('-createdAt _id');
-    // }
-
-    // 3) Field limiting
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select('-__v');
-    // }
-
-    // // 4) Pagination
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit;
-
-    // // ex: page=2&limit=10
-    // query = query.skip(skip).limit(limit);
-
-    // if (req.query.page) {
-    //   const numTours = await Tour.countDocuments();
-    //   if (skip >= numTours) throw new Error('This page does not exist');
-    // }
-
     // EXECUTE QUERY
     /* According to our class definition: 
     1. query is the mongoose query hence Tour.find()
